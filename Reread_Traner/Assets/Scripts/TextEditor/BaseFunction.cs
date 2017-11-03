10b8813ad54dae2dbe7a0629a0c5eeedfe4509be
Ailexe <fkc.ailexe@gmail.com>
2017-11-03 19:50:50 +0900

    - start study project
    word edit tool for my study

---
+++ Reread_Traner/Assets/Scripts/TextEditor/BaseFunction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//デバック
+using UnityEngine.UI;
+
+public class BaseFunction : MonoBehaviour {
+	float width; //実機の横幅
+	float height; //実機の縦幅
+
+	public InputField input_field; //テキストエリア
+
+	// Use this for initialization
+	void Start () {
+		learn_input_field();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//初期設定
+	void init(){
+		//値の挿入
+		width = (float)Screen.currentResolution.width;
+		height = (float)Screen.currentResolution.height;
+	}
+
+	void learn_input_field(){
+		input_field.asteriskChar = "$!£%&*"[0];
+	}
+}
