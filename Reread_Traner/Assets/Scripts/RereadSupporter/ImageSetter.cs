7a874606d322fda7aebc2e6f621c82c250786a89
Ailexe <fkc.ailexe@gmail.com>
2017-11-07 20:21:18 +0900

    - set effect in environment layer

--- Reread_Traner/Assets/Scripts/RereadSupporter/ImageSetter.cs
+++ Reread_Traner/Assets/Scripts/RereadSupporter/ImageSetter.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //振動effectに使用するオブジェクトの設定を行うクラス
 public class ImageSetter : MonoBehaviour {
 
 	public Canvas ImageCanvas; //親キャンバス
+	float height; //実機の縦幅
+	float size; //Imageのサイズ
 
 
 
@@ -20,7 +23,18 @@ public class ImageSetter : MonoBehaviour {
 	}
 
 	void ImageSetting(string image_path){
-		GameObject img = ImageCanvas.transform.Find("Image").gameObject;
-		Debug.Log(image_path);
+		height = (float)Screen.height;
+
+		size = height/15.0F;
+		
+		GameObject Img = ImageCanvas.transform.Find("Image").gameObject; //Canvas直下のImageを取得
+		Img.GetComponent<Image>().sprite = Resources.Load<Sprite>("Image/CROSS"); //Imageを指定された画像に設定
+		Img.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size); //Imageのサイズ指定
+		CanvasSetting(size);
+		//Debug.Log(image_path);
+	}
+
+	void CanvasSetting(float _size){
+		ImageCanvas.GetComponent<RectTransform>().sizeDelta = new Vector2(_size, _size); //Canvasのサイズ指定
 	}
 }
--- Reread_Traner/Assets/Scripts/RereadSupporter/TypeEnviron.cs
+++ Reread_Traner/Assets/Scripts/RereadSupporter/TypeEnviron.cs
@@ -6,9 +6,11 @@ public class TypeEnviron : SupportType {
 
 	public Canvas EffectCanvas;
 
+	const int OBJ_NUM = 8;
+
 	// Use this for initialization
 	void Start () {
-		
+		set_object();
 	}
 	
 	// Update is called once per frame
@@ -18,6 +20,23 @@ public class TypeEnviron : SupportType {
 
 	//視線誘導に用いるオブジェクトを配置
 	void set_object(){
-		
+		float width = (float)Screen.width; //実機の横幅
+		float height = (float)Screen.height; //実機の縦幅
+		float posX = (width * 5.0f)/12.0f; //振動体の横位置
+		float posY = height / (float)OBJ_NUM; //振動体の高さの間隔
+
+		for(int i=0; i<2; i++){
+			for(int j=0; j<OBJ_NUM; j++){
+				GameObject effect = (GameObject)Resources.Load("Prefabs/Effect/EnvironEffectImage");
+				GameObject prefab = (GameObject)Instantiate(effect);
+				prefab.transform.SetParent(EffectCanvas.transform, false);
+				if(i==0){
+					prefab.GetComponent<RectTransform>().localPosition = new Vector3(posX, posY * j - posY * (OBJ_NUM / 2.0f) + posY / 2.0f, 0.0f);
+				}
+				else if(i==1){
+					prefab.GetComponent<RectTransform>().localPosition = new Vector3(posX * (-1.0f), posY * j - posY * (OBJ_NUM / 2.0f) + posY / 2.0f, 0.0f);
+				}
+			}
+		}
 	}
 }
