cc162cd9e120dc9479682830911a5f8498fd2ce1
Ailexe <fkc.ailexe@gmail.com>
2017-11-17 16:08:00 +0900

    - implement calcurate edit position method

--- Reread_Traner/Assets/Scripts/TextEditor/BaseFunction.cs
+++ Reread_Traner/Assets/Scripts/TextEditor/BaseFunction.cs
@@ -11,9 +11,9 @@ public class BaseFunction : MonoBehaviour {
 
 	public InputField input_field; //テキストエリア
 
-	private TextInfo handled; //編集前のテキストを保持
-
 	private int char_num; //改行文字数
+
+	private float font_size; //フォントサイズ
 
 	// Use this for initialization
 	void Start () {
@@ -31,8 +31,8 @@ public class BaseFunction : MonoBehaviour {
 		//height = (float)Screen.currentResolution.height;
 		width = (float)Screen.width;
 		height = (float)Screen.height;
-		set_size(width, height); //editor領域の指定
 		set_type(); //編集領域の詳細な設定
+		set_size(width, height); //editor領域の指定
 	}
 
 	//editor領域のサイズを指定する関数
@@ -40,7 +40,7 @@ public class BaseFunction : MonoBehaviour {
 		//引数で渡された画面サイズをもとにeditor領域のサイズを設定
 		float window_w = (w * 2.0F) / 3.0F;
 		float window_h = h;
-		char_num = (int)(window_w / 14.0F) - 2; //一行あたりの文字数を計算
+		char_num = (int)(window_w / font_size) - 2; //一行あたりの文字数を計算
 		//Debug.Log(char_num);
 
 		//設定したeditor領域のサイズに変更
@@ -53,6 +53,8 @@ public class BaseFunction : MonoBehaviour {
 		input_field.GetComponent<InputField>().lineType = InputField.LineType.MultiLineNewline; //改行あり
 		input_field.GetComponent<InputField>().contentType = InputField.ContentType.Standard; //全ての文字入力可
 		input_field.GetComponent<InputField>().transition = InputField.Transition.None; //インラタクティブなし
+		font_size = (float)input_field.transform.Find("Text").GetComponent<Text>().fontSize; //フォントサイズを取得
+		Debug.Log(font_size);
 	}
 
 	//テキストの変更を検知
@@ -60,17 +62,8 @@ public class BaseFunction : MonoBehaviour {
 		Debug.Log("OnValue：" + input_field.text);
 		char[] delimiterChars = { '\n'}; // Splitの対象文字列
 		string[] text = input_field.text.Split(delimiterChars); //改行位置をスプリットして配列を生成
-		int[] indent = new int[text.Length]; //それぞれの文字列が費やす行数を格納
-		//文字列ごとの行数の計算
-		for(int i=0; i<indent.Length; i++){
-			indent[i] = text[i].Length / char_num;
-			if(text[i].Length % char_num != 0 || text[i].Length == 0){
-				indent[i] += 1;
-			}
-		}
-		handled.text = text;
-		handled.indent = indent;
-		
+		int caret = input_field.caretPosition; //編集位置までに入力されている文字数
+		Debug.Log(calcu_edit_pos(caret, text));
 	}
 
 	//テキストの確定を検知
@@ -79,34 +72,34 @@ public class BaseFunction : MonoBehaviour {
 	}
 
 	//編集位置の座標を推定する関数（handled：直前の文字列情報, recent：最新の文字列情報）
-	Vector2 caluc_edit_pos(TextInfo handled, TextInfo recent){
-		Vector2 result = new Vector2(0.0f, 0.0f); //計算結果
-		if(handled.indent.Length >= recent.indent.Length){
-			//文字列が削除された場合
-			for(int i=0; i<recent.indent.Length; i++){
-				if(recent.text[i] != handled.text[i]){
-					//文字列が一致しなかった場合
-
+	Vector2 calcu_edit_pos(int caret, string[] text){
+		Vector2 result = new Vector2(0.0f, 0.0f);
+		int indent = 0; //編集位置までの改行回数をカウントする
+		int num = 0; //編集位置までに費やす文字数
+		int temp_row = 0; //文字列が要する行数を一時的に格納する変数
+		Debug.Log(text.Length);
+		for(int i=0; i<text.Length; i++){
+			num = text[i].Length;
+			if( num < caret ){
+				//numがcaretより小さい場合
+				caret = caret - ( num + 1 ); //caretを文字数分引く
+				temp_row = Mathf.CeilToInt( num / char_num ); //要した行数を計算
+				indent = indent + temp_row;
+			}else{
+				if( caret > char_num ){
+					//caretが一行あたりの文字数をオーバーした場合
+					caret = caret % char_num; //横からの文字数を計算
+					indent = indent + caret / char_num + 1; //行数を計算
+					break;
+				}else{
+					//caretが一行あたりの文字数をオーバーしなかった場合
+					break;
 				}
-				//文字列の不一致がなかった場合
 			}
-		}else{
-			//文字列が追加された場合
-			for(int i=0; i<handled.indent.Length; i++){
-				if(recent.text[i] != handled.text[i]){
-					//文字列が一致しなかった場合
-
-				}
-			}
-			//文字列の不一致がなかった場合
+			indent += 1;
 		}
+		result = new Vector2(caret * font_size, indent * font_size);
 		return result;
 	}
 }
 
-
-//テキストの情報を保持するクラス
-class TextInfo{
-	public string[] text; //文字列
-	public int[] indent; //行数
-}
